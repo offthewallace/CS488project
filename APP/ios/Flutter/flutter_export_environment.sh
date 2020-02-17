c61203491dbefd9adb7a6be868d566735d47b785
Wallace He <guanlin.he@richmond.edu>
2020-02-16 23:25:32 -0500

    Update for the login on android
    Co-Authored-By: ET <et-777@users.noreply.github.com>
    Co-Authored-By: anyuchen1234 <anyuchen1234@users.noreply.github.com>

--- APP/ios/Flutter/flutter_export_environment.sh
+++ APP/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Tencent\sdk\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Guanlin he\Documents\GitHub\FlutterWhatsAppClone"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/wallace/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/wallace/AndroidStudioProjects/flutter_app"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Tencent\sdk\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/wallace/Documents/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
