80c8740c4219199fa314b91469aab8f8a00c188a
offthewallace <gbh5146@psu.edu>
2020-03-15 23:47:20 -0400

    Adding the swipe on the APP
    Adding the swipe on the APP for demo. modified by Guanlin He, Yuchen and Eugen.
    
    Co-Authored-By: ET <et-777@users.noreply.github.com>
    Co-Authored-By: anyuchen1234 <anyuchen1234@users.noreply.github.com>

--- APP/lib/main.dart
+++ APP/lib/main.dart
@@ -1,45 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'home.dart';
-import 'login-register.dart';
+import 'swipe_feed_page.dart';
 
-void main() {
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
-    runApp(new MyApp());
-  });
-}
-
-final FirebaseAuth auth = FirebaseAuth.instance;
-
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    
+
+
+   build(BuildContext context) {
     return MaterialApp(
-      title: 'GO',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          hintColor: Color(0xFFC0F0E8),
-          primaryColor: Color(0xFF80E1D1),
-          fontFamily: "Montserrat",
-          canvasColor: Colors.transparent),
-      home: new StreamBuilder(
-        stream: auth.onAuthStateChanged,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Home();
-          }
-          return LoginRegister();
-        },
-      ),
-      routes: <String, WidgetBuilder>{
-        '/home': (BuildContext context) => new Home(),
-        '/login': (BuildContext context) => new LoginRegister()
-      },
+      title: 'Tinder cards demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SwipeFeedPage(),
     );
   }
 }
