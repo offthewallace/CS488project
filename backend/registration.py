9388820ef755296cdaf430758f4c3251268e06f2
ET <eugenio_tijerina@hotmail.com>
2020-04-21 18:28:45 -0500

    backend change (minor)
    forgot to upload backend file

--- backend/registration.py
+++ backend/registration.py
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
     #print()
     s3_client.put_item(TableName='Registration', Item={'username':{'S':res['email']},'passwords':{'S':res['password']}})
     #s3_client.put_item(TableName='UserTable2',Item={'username':{'S':res['email']},'displayname':{'S':res['name']+' '+res['surname']},'gender':{'S':res['gender']},'country':{'S':res['country']},'city':{'S':res['city']},'birth':{'S':res['birth']}})
-    s3_client.put_item(TableName='UserTable',Item={"username":{'S':res['email']}, "name":{'S':res['name']},'surname':{'S':res['surname']}, 'password':{'S':res['password']},'password2':{'S':res['password2']},'gender':{'S':res['gender']},'country':{'S':res['country']},'city':{'S':res['city']},'birth':{'S':res['birth']}, 'like':{'S':" "},'bio':{'S':' '}})
+    s3_client.put_item(TableName='UserTable',Item={"username":{'S':res['email']}, "name":{'S':res['name']},'surname':{'S':res['surname']}, 'password':{'S':res['password']},'password2':{'S':res['password2']},'gender':{'S':res['gender']},'country':{'S':res['country']},'city':{'S':res['city']},'birth':{'S':res['birth']}, 'like':{'S':" "},'bio':{'S':' '}, 'image':{'S':res['image']} })
     return {
         'statusCode': 200,
         'body': json.dumps('test work!!!!')
